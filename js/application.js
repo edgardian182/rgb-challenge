fe4857d477658d29f4001045c9b09f0a5dceac3a
Edgar Quintana <edgarquintana984@hotmail.com>
2016-11-08 10:30:39 -0500

    rgb challenge

---
+++ js/application.js
@@ -0,0 +1,50 @@
+var score = 0;
+var correcto;
+
+// Numero aleatorio entre 0 y 255
+function random() {
+  return Math.floor(Math.random()*256);
+}
+
+function generateColor(){
+  return 'rgb(' + random() + ', ' + random() + ', ' + random() + ')'
+}
+
+game();
+
+function game(){
+  correcto = Math.floor(Math.random() * 2)
+
+  $('.color').each(function(index){
+    var color = generateColor()
+    $(this).css('background-color', color);
+    console.log(color);
+    if (index == correcto) {
+      $('#correcto').text(color);
+    }
+  })
+}
+
+$('.color').on('click tap', function(){
+  // Encontramos el indice del elemento al que se le dio click
+  var index = $('.color').index(this);
+  // Si este indice es igual al definido como correcto en el game =>
+  if (index == correcto) {
+    score ++;
+    $('.result.won').show();
+  }
+  else {
+    score = 0;
+    $('.result.lost').show()
+  }
+  $(this).addClass('scale');
+  $('.score span').text(score);
+  $('.result #resultado').text("SCORE: " + score);
+})
+
+$('.result a').on('click tap', function(){
+  $('.color').removeClass('scale')
+  $('.result').hide();
+  game();
+})
+
